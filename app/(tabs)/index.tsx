8401a55044881246e3796da93cdc41fdfbd6605b
gottajsx <gottajsx@gmail.com>
2024-10-07 22:17:36 +0200

    Bottom Row Component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,18 +5,18 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-
-import { CameraView } from 'expo-camera';
+import { CameraMode, CameraView } from 'expo-camera';
 import * as WebBrowser from 'expo-web-browser';
+import BottomRowTools from '@/components/BottomRowTools';
 
 export default function HomeScreen() {
   const cameraRef = React.useRef<CameraView>(null)
+  const [cameraMode, setCameraMode] = React.useState<CameraMode>("picture")
   return (
     <View style={{flex: 1}}>
-      <CameraView
-        ref={cameraRef}
-        style={{flex:1}}
-      />
+      <CameraView ref={cameraRef} mode={cameraMode} style={{flex:1}}>
+        <BottomRowTools setCameraMode={setCameraMode} cameraMode={cameraMode}/>
+      </CameraView>
     </View>
   );
 }
